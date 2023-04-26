a0b268debd0dc27269a9218678909cbe0dc45d27
alexbrenes999 <116224156+alexbrenes999@users.noreply.github.com>
2023-04-25 21:51:40 -0400

    change the structure of the file as well as got the server side working

---
+++ client/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client'
+import './index.css';
+import App from './App';
+import client from './graphql/apolloClient.js';
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/authContext';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(
+    <AuthProvider>
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <React.StrictMode>
+                    <App />
+                </React.StrictMode>
+            </BrowserRouter>
+        </ApolloProvider>
+    </AuthProvider>
+);
