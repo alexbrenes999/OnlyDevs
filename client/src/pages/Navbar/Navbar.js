49496747c93d96f111fc13deeec1d03310f859e6
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2023-04-27 19:11:31 -0500

    added class for dynamically changing profile information and user profile pic in nav bar

--- client/src/pages/Navbar/Navbar.js
+++ client/src/pages/Navbar/Navbar.js
@@ -70,9 +70,9 @@ export default function Example() {
                     <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open user menu</span>
                       <img
-                        className="h-12 w-12 rounded-full"
+                        className="h-12 w-12 rounded-full nav-profile"
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt="Photo of john doe"
+                        alt="User profile picture"
                       />
                     </Menu.Button>
                   </div>
