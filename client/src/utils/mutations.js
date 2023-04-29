472e58f9f4e3935272af5c5296a45851fe96918a
alexbrenes999 <116224156+alexbrenes999@users.noreply.github.com>
2023-04-29 13:00:07 -0400

    login connected to the front end. TODO 4 Frontend: reroute to the profile page

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,3 +13,15 @@ mutation registerUser($firstName: String!, $lastName: String!, $username: String
   }
 }
 `;
+
+export const LOGIN_USER = gql`
+mutation loginUser($username: String!, $password: String!) {
+  loginUser(username: $username, password: $password) {
+    token
+    user {
+      _id
+      username
+    }
+  }
+}
+`
